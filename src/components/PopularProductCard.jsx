57a4c8d644194ea740dae2a3ab274a7e9d8eccf7
Kapil15 <Chaudhary99Kapil@gmail.com>
2023-08-25 14:30:31 +0530

    responsive check

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -5,11 +5,11 @@ const PopularProductCard = ({imgURL, name, price}) => {
     <>
     <div className='flex flex-1 flex-col w-full max-sm:w-full'>
      <img src={imgURL} alt={name} className='w-[280px] h-[280px]' />
-    </div>
     
     <div className='mt-8 flex justify-start gap-2.5'>
       <img src={star} alt="rating" width={24} height={24}/>
       <p className="font-montserrat leading-normal text-xl text-slate-gray">(4.7)</p>
+    </div>
       <h3 className="mt=2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
       <p className="mt-2 text-2xl text-coral-red font-semibold font-montserrat leading-normal">{price}</p>
     </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,15 +11,17 @@ const Hero = () => {
    const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
 
   return (
-   <section id="home" className="w-full gap-10 max-container flex xl:flex-row justify-center min-h-screen py-3">
+   <section id="home" className="w-full gap-10 max-container flex xl:flex-row justify-center min-h-screen">
     <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
       <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
       <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold"><span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
       <br />
-      <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+      <span className="text-coral-red inline-block mt-3">NikeRun</span> Shoes
       </h1>
       <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality Comfort, and innovation for your active life.</p>
-      <Button label="Show now" iconURL={arrowRight}/> 
+      <Button label="Shop now" iconURL={arrowRight}/> 
+
+
       <div className="flex justify-start item-start flex-wrap w-full mt-20 gap-16">
         {statistics.map((stat, index) =>(
          <div key={index}>
@@ -30,7 +32,7 @@ const Hero = () => {
       </div>
     </div>
 
-    <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-10 bg-primary">
+    <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary">
       <img src={bigShoe1} alt="shoes" width={610} height={500} className="object-contain relative z-10"/>
       <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%]  max-sm:px-6">
         {
